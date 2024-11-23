adde26800edf9149305ca70e604c872d7f20a67d
Sayak Pradhan <pradhansayak2002@gmail.com>
2024-11-23 13:32:48 +0530

    week 1 - Home work done (Alice Bakery

---
+++ src/main/java/com/codeshuttle/cakebakerapplication/cakebakerapplication/CakeBaker.java
@@ -0,0 +1,23 @@
+package com.codeshuttle.cakebakerapplication.cakebakerapplication;
+
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CakeBaker {
+
+    private final Frosting frosting;
+    private final Syrup syrup;
+
+    public CakeBaker(Frosting frosting, Syrup syrup) {
+        this.frosting = frosting;
+        this.syrup = syrup;
+    }
+
+    public void bakeCake() {
+
+        System.out.println(frosting.getFrostingType() + " " + syrup.getSyrupType());
+
+    }
+
+}
